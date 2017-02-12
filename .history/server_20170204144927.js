b84b7bf32e6a989afd23e616fbf08b9ae2ed6997
Yusuf Kelo <yusufkelo@Yusufs-MacBook-Pro.local>
2017-02-13 00:59:18 +0400

    update

---
+++ .history/server_20170204144927.js
@@ -0,0 +1,35 @@
+// server.js
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var router = jsonServer.router('db.report.json');
+var properties = jsonServer.router('db.properties.json')
+var middlewares = jsonServer.defaults();
+var express = require('express'),
+    cors = require('cors'),
+    app = express();
+
+
+
+// app.use(cors());
+
+
+
+// var app = express();
+// // ...
+
+// // You may want to mount JSON Server on a specific end-point, for example /api
+// // Optiona,l except if you want to have JSON Server defaults
+// // server.use('/api', jsonServer.defaults()); 
+// app.use(router);
+
+// app.listen(5000, function() {
+//     console.log('server started..')
+// });
+
+
+server.use(middlewares)
+server.use(router)
+server.use(properties)
+server.listen(5000, function() {
+    console.log('JSON Server is running')
+})
